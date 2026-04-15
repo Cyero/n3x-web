5d1d97633b1269123757eeb06118b69f0ac1d7b1
Anton Butkov <ant.butkov@gmail.com>
2026-04-15 23:06:46 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e
+
 mkdir -p dist
 cp n3x.html dist/n3x.html
-sed -i "s|__HAPP_LINK__|${HAPP_LINK}|g" dist/n3x.html
-sed -i "s|__TOKEN__|${TOKEN}|g" dist/n3x.html
+
+file="dist/n3x.html"
+
+content=$(cat "$file")
+
+content="${content//__HAPP_LINK__/$HAPP_LINK}"
+content="${content//__TOKEN__/$TOKEN}"
+
+echo "$content" > "$file"
