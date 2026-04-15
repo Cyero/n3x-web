24c1f36186095db1219c362fd7d22138f7716823
Anton Butkov <ant.butkov@gmail.com>
2026-04-15 22:25:38 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p dist
 cp n3x.html dist/n3x.html
-sed "s|{{__HAPP_LINK__}}|$HAPP_LINK|g" dist/n3x.html > dist/n3x.tmp && mv dist/n3x.tmp dist/n3x.html
-sed "s|{{__TOKEN__}}|$TOKEN|g" dist/n3x.html > dist/n3x.tmp && mv dist/n3x.tmp dist/n3x.html
+sed -i "s|__HAPP_LINK__|${HAPP_LINK}|g" dist/n3x.html
+sed -i "s|__TOKEN__|${TOKEN}|g" dist/n3x.html
