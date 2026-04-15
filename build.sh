d5569a035b8bcde05af3e0d8dc3198562def6826
Anton Butkov <ant.butkov@gmail.com>
2026-04-15 21:43:10 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sed -i "s|__HAPP_LINK__|${HAPP_LINK}|g" n3x.html
-sed -i "s|__TOKEN__|${TOKEN}|g" n3x.html
+mkdir -p dist
+cp n3x.html dist/n3x.html
+sed -i "s|__HAPP_LINK__|${HAPP_LINK}|g" dist/n3x.html
+sed -i "s|__TOKEN__|${TOKEN}|g" dist/n3x.html
+ 
